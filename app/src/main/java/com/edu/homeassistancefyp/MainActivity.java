6b30929041cb9a1b60714161ae4e3405044207da
12 <21>
2019-07-01 00:30:23 +0500

    Home Assistance 1.0

---
+++ app/src/main/java/com/edu/homeassistancefyp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.edu.homeassistancefyp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent i = new Intent(this,LoginInterface.class);
+        startActivity(i);
+    }
+}
