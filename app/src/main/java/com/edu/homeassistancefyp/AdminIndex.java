3cd7415b08d5e83602cc7a093ef31706c001540f
talha <talha.nobeen18@gmail.com>
2019-07-02 00:42:05 +0500

    admin module

---
+++ app/src/main/java/com/edu/homeassistancefyp/AdminIndex.java
@@ -0,0 +1,17 @@
+package com.edu.homeassistancefyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AdminIndex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_index);
+    }
+    public  void languageChangeButton(View v){
+        new Language(this).ShowChangeLanguage();
+    }
+}
