38e48b304f832ff44ca025c7e5a031415bcd87e1
talhanobeen <talha.nobeen18@gmail.com>
2019-09-22 00:19:13 +0500

    sae wala code yeh ha

---
+++ app/src/main/java/com/edu/homeassistancefyp/WorkerIndex.java
@@ -0,0 +1,13 @@
+package com.edu.homeassistancefyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkerIndex extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_index);
+    }
+}
